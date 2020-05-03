0563cb52f8da5fcde88f1b8bc3895d29a9e7e921
Guyoguyes <g.abuba43@gmail.com>
2020-05-03 13:58:20 +0300

    used array to order pizza

--- pratical1/src/Order.java
+++ pratical1/src/Order.java
@@ -4,11 +4,11 @@ public class Order {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Pizza order1 = new Pizza("large", "soft", "pineapple");
-
-
-        double priceOrder1 = order1.makeOrder();
-        System.out.println(priceOrder1);
+//        Pizza order1 = new Pizza("large", "soft", "pineapple");
+//
+//
+//        double priceOrder1 = order1.makeOrder();
+//        System.out.println(priceOrder1);
 
 
         Pizza order2 = new Pizza(input.next(), input.next(), input.next());
--- pratical1/src/Pizza.java
+++ pratical1/src/Pizza.java
@@ -13,37 +13,72 @@ public class Pizza {
     public double makeOrder(){
         System.out.println("Making Pizza Order");
         System.out.println("Pizza Size");
-        String large = "large";
-        String medium = "medium";
-        String small = "small";
-
-        if(this.size.equals(large.toLowerCase())){
-            this.price = this.price + 30;
-        }else if (this.size.equals(medium.toLowerCase())){
-            this.price = this.price + 50;
-        }else if (this.size.equals(small.toLowerCase())){
-            this.price = this.price + 70;
+        String[] pizzaSize = {"large", "medium", "small"};
+        int i;
+        for(i = 0; i < pizzaSize.length; i++) {
+            System.out.println(pizzaSize[i]);
         }
+            if(this.size.equals(pizzaSize[0].toLowerCase())){
+                this.price = this.price + 30;
+            }else if(this.size.equals(pizzaSize[1].toLowerCase())){
+                this.price = this.price + 50;
+            }else if (this.size.equals(pizzaSize[2].toLowerCase())){
+                this.price = this.price + 70;
+            }else{
+                System.out.println("Pizza size is required");
+            }
+
+
+//        if(this.size.equals(large.toLowerCase())){
+//            this.price = this.price + 30;
+//        }else if (this.size.equals(medium.toLowerCase())){
+//            this.price = this.price + 50;
+//        }else if (this.size.equals(small.toLowerCase())){
+//            this.price = this.price + 70;
+//        }
 
         System.out.println("Pizza Crust");
-        String hard = "hard";
-        String soft = "soft";
+        String[] pizzaCrust = {"crispy", "stuffed", "Gluten-free"};
 
-        if(this.crust.equals(hard.toLowerCase())){
-            this.price = this.price + 10;
-        }else if(this.crust.equals(soft.toLowerCase())){
-            this.price = this.price + 20;
+        for(i=0; i<pizzaCrust.length; i++){
+            System.out.println(pizzaCrust[i]);
         }
+        if(this.crust.equals(pizzaCrust[0].toLowerCase())){
+                this.price = this.price + 10;
+            }else if(this.crust.equals(pizzaCrust[1].toLowerCase())){
+                this.price = this.price + 15;
+            }else if(this.crust.equals(pizzaCrust[2].toLowerCase())){
+                this.price = this.price + 20;
+            }
+
+//        if(this.crust.equals(hard.toLowerCase())){
+//            this.price = this.price + 10;
+//        }else if(this.crust.equals(soft.toLowerCase())){
+//            this.price = this.price + 20;
+//        }
 
         //topping
-        String pineapple = "pineapple";
-        String mongo = "mongo";
+        String[] pizzaTopping = {"Bacon", "Chicken", "Beef", "Vegetables"};
 
-        if(this.topping.equals(pineapple.toLowerCase())){
-            this.price = this.price + 15;
-        }else if(this.topping.equals(mongo.toLowerCase())){
-            this.price = this.price + 23;
+        for (i=0; i < pizzaTopping.length; i++) {
+            System.out.println(pizzaTopping[i]);
         }
+            if(this.topping.equals(pizzaTopping[0].toLowerCase())){
+                this.price = this.price + 15;
+            }else if(this.topping.equals(pizzaTopping[1].toLowerCase())){
+                this.price = this.price + 20;
+            }else if(this.topping.equals(pizzaTopping[2].toLowerCase())){
+                this.price = this.price + 25;
+            }else if (this.topping.equals(pizzaTopping[3].toLowerCase())){
+                this.price = this.price + 10;
+            }
+
+
+//        if(this.topping.equals(pineapple.toLowerCase())){
+//            this.price = this.price + 15;
+//        }else if(this.topping.equals(mongo.toLowerCase())){
+//            this.price = this.price + 23;
+//        }
 
         return this.price;
     }
